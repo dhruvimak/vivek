1f48a4e5189e939f678d428bce9fdba5898ffca9
dhruvimak <dhruvipmakavana@yahoo.com>
2023-02-04 01:20:35 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener('load', (event) => {
-    var date = new Date();
-    var currentYear = date.getFullYear();
-    var dateEle = document.getElementById('currentYear');
-    dateEle.textContent = currentYear;
-});
