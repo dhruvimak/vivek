2698c2debfd9ac5b4fc3835a21b92aa817db3c17
d0m0944 <dhruvi.makvana@walmart.com>
2023-03-20 00:23:00 +0530

    slider fixes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
